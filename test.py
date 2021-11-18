eb78db1e391acd128c1673e99904c20d424aa8bd
George Mantellos <george.mantellos@pdmfc.com>
2021-11-18 15:51:21 +0100

    updates for doc purposes

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from .tests.test_lib import defineArguments, processInput
 from .tests.test_bm import test_bm
+from .tests.test_new_test import new_test
 
 # Write Here all the available tests you want to run
 availableTests = {
     "business:test": test_bm,
+    "business:new_test": new_test,
 }
 
 
