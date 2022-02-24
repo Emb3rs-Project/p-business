f35a014ebc02325799f0fa73051ee19c3afcc620
Bilal Siddique Khan <bsikh@dtu.dk>
2022-02-24 11:26:39 +0000

    Update test_heat_rec.py

--- tests/test_heat_rec.py
+++ tests/test_heat_rec.py
@@ -9,7 +9,7 @@ def test_int_heat():
         "discount_rate" : 4, # discount rate / float
         "money_sav" : 300, # energy saved in eur per year due to dispatch/ number, integer/float
         "carbon_sav_quant": 200, # yearly carbon savings kg/MWh / number, integer/float
-        "carbon_sav_money": 200, # yearly carbon savings eur / number, integer/float
+ #       "carbon_sav_money": 200, # yearly carbon savings eur / number, integer/float !!! not implemented
         "duration": 9  # project duration / number, integer
         }
     int_heat_output = int_heat_rec(heat_rec_input_dict)
